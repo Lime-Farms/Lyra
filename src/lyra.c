61e7b34d4bb61f6555e311aac36b329e49d3bb60
Nick Chambers <nchambers@lightspeedsystems.com>
2021-04-28 08:17:32 -0500

    Collect environment variables into a hashmap

--- src/lyra.c
+++ src/lyra.c
@@ -1,3 +1,27 @@
-int main() {
-  return 42;
+#include <errno.h>
+#include <hm.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv, char **envs) {
+  int arg = opterr = 0;
+
+  while((arg = getopt(argc, argv, "")) > 0) {
+    if(arg == '?') {
+      fprintf(stderr, "error: option '-%c' incorrectly used.\n", arg);
+      return EINVAL;
+    }
+  }
+
+  char **env = envs;
+  struct hm map = hm_new(0);
+
+  for(; *env != NULL; env += 1) {
+    const char *name = strtok(*env, "=");
+    size_t len = strlen(name);
+    hm_add(&map, name, *env + len + 1);
+  }
+
+  return 0;
 }
